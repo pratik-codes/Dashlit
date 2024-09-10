f1ec489d310bb636e95b3d784e09e1cf5bc74d20
Himanshu8747 <himanshu8747@gmail.com>
2024-09-10 10:49:37 +0530

    Remove rtkuery

--- apps/extension/entrypoints/newtab/main.tsx
+++ apps/extension/entrypoints/newtab/main.tsx
@@ -2,13 +2,9 @@ import App from "@/components/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "~/assets/tailwind.css";
-import store from "@/store/store";
-import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}> 
       <App />
-    </Provider>
   </React.StrictMode>,
 );
