b92545fccc268cee5357ebcfb8be14448507ff8d
Himanshu8747 <himanshu8747@gmail.com>
2024-09-18 17:18:02 +0530

    Fix Indentation

--- apps/extension/components/home/Links/SearchLinks.tsx
+++ apps/extension/components/home/Links/SearchLinks.tsx
@@ -1,16 +1,14 @@
-import { Input } from "@/components/ui/input"
+import { Input } from "@/components/ui/input";
 
 const SearchLinks = () => {
   return (
     <div className="space-y-2 mx-3">
-  <Input
-    className="rounded-[12px] font-bold px-3 py-2 text-lg text-white w-full placeholder-white placeholder-opacity-50 bg-transparent border-4 border-gray-500 focus:border-purple-500 focus:outline-none"
-    placeholder="Search links"
-  />
-</div>
+      <Input
+        className="rounded-[12px] font-bold px-3 py-2 text-lg text-white w-full placeholder-white placeholder-opacity-50 bg-transparent border-4 border-gray-500 focus:border-purple-500 focus:outline-none"
+        placeholder="Search links"
+      />
+    </div>
+  );
+};
 
-
-  )
-}
-
-export default SearchLinks
+export default SearchLinks;
